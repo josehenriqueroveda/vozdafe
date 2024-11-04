c12bbe453b8a88591376f7d3880d7bc6b0f06308
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-11-04 19:30:32 +0000

    feat: add homepage

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <p>"Eu lhes disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo." João 16:33</p>
+}
+
+export default Home;
