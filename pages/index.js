0a0a67d18714d2fa6e0e32fd9f8d8b640699025c
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-11-05 17:38:53 +0000

    feat: update verse tag

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <p>"Eu lhes disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo." João 16:33</p>
+    return <h2>"Eu lhes disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo." <b>João 16:33</b></h2>
 }
 
 export default Home;
