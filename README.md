b4b1a894ef5aca844a2758e2daebb65d63bdf22a
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-11-07 17:58:22 +0000

    feat: add scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # church-news
+
 Conteúdos e notícias para a sua igreja.
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
   return (
     <div>
-      <h2>"Eu lhes disse essas coisas para que em mim vocês tenham paz. Neste mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo."</h2>
+      <h2>
+        "Eu lhes disse essas coisas para que em mim vocês tenham paz. Neste
+        mundo vocês terão aflições; contudo, tenham ânimo! Eu venci o mundo."
+      </h2>
       <h3>João 16:33</h3>
     </div>
-  )
+  );
 }
 
 export default Home;
